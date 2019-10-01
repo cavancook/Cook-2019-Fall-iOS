7fcdf6774c4bbca6ec7531a11adb698ddf966598
Cavan Cook <cavanmcook@gmail.com>
2019-10-01 17:53:52 -0600

    Adding HW 4

---
+++ Homework2/Homework2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Cavan Cook on 9/12/19.
+//  Copyright © 2019 Cavan Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
