bd8738f691fc788a4e5aa3c18c5cf169ffcc7c55
Cavan Cook <cavanmcook@gmail.com>
2019-11-20 20:16:32 -0700

    More work on Final and added pdfs for HW2 and 3

---
+++ FinalProject/ParkItBoi/ParkItBoi/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ParkItBoi
+//
+//  Created by Cavan Cook on 11/19/19.
+//  Copyright © 2019 Cavan Cook. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
---
+++ FinalProject/ParkItBoi/ParkItBoi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParkItBoi
+//
+//  Created by Cavan Cook on 11/19/19.
+//  Copyright © 2019 Cavan Cook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
